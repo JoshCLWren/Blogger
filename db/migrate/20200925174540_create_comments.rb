e4f772b9c028a906bf29e16af41f31fbe8af3662
Josh Wren <josh.wren@life.church>
2020-09-25 13:06:37 -0500

    Implemented comments
    -generated comment controller
    -added functinality to add comments while in show view of article

--- db/migrate/20200925174540_create_comments.rb
+++ db/migrate/20200925174540_create_comments.rb
@@ -1,6 +1,6 @@
-class CreateCommets < ActiveRecord::Migration[6.0]
+class CreateComments < ActiveRecord::Migration[6.0]
   def change
-    create_table :commets do |t|
+    create_table :comments do |t|
       t.string :author_name
       t.text :body
       t.references :article, null: false, foreign_key: true
