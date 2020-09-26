d5d9c1cf306e750dfd00f3e4b11298d171ed2fcb
Josh Wren <josh.wren@life.church>
2020-09-26 15:08:03 -0500

    Added destroy capabilities for articles and tags
    -fixed bug with articles not being deleted
    -preemptivily fixed the same bug with tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,4 +6,12 @@ class TagsController < ApplicationController
   def index
     @tags = Tag.all
   end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    flash.notice = "tag '#{@tag.name}' destroyed!"
+
+    redirect_to tags_path
+  end
 end
